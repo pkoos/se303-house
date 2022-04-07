89edc0eb98ef3877d052a87d5cee15bcb46b71aa
Paul Koos <paully_k5298@yahoo.com>
2022-04-07 10:23:14 -0700

    pass test_line_2.

--- lib/house.rb
+++ lib/house.rb
@@ -1,5 +1,9 @@
 class House
   def line(argument)
-    "This is the house that Jack built.\n"
+    if argument == 1
+      result = "This is the house that Jack built.\n"
+    else
+      "This is the malt that lay in the house that Jack built.\n"
+    end
   end
 end
