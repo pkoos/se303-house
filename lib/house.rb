464590e725d4d20ae3ecb4c5574371b38bd51dd8
Paul Koos <paully_k5298@yahoo.com>
2022-04-18 12:01:04 -0700

    carry through abstraction to the rest of the code.

--- lib/house.rb
+++ lib/house.rb
@@ -4,6 +4,13 @@ class House
   def initialize
     @lyrics = [
       'This is ',
+      'the horse and the hound and the horn that belonged to ',
+      'the farmer sowing his corn that kept ',
+      'the rooster that crowed in the morn that woke ',
+      'the priest all shaven and shorn that married ',
+      'the man all tattered and torn that kissed ',
+      'the maiden all forlorn that milked ',
+      'the cow with the crumpled horn that tossed ',
       'the dog that worried ',
       'the cat that killed ',
       'the rat that ate ',
@@ -13,25 +20,7 @@ class House
   end
 
   def line(argument)
-    case argument
-    when 1..5
-      result = "#{@lyrics.first}#{@lyrics.last(argument).join('')}"
-    when 6
-      result = "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    when 7
-      result = "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    when 8
-      result = "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    when 9
-      result = "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    when 10
-      result = "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    when 11
-      result = "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    else
-      result = "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    end
-    result
+    "#{@lyrics.first}#{@lyrics.last(argument).join('')}"
   end
 
   def recite
