34d269ea03769dbde65b0f3f0ad937754f170c95
Paul Koos <paully_k5298@yahoo.com>
2022-05-30 13:54:26 -0700

    remove case when argument is 8.

--- lib/house.rb
+++ lib/house.rb
@@ -3,7 +3,8 @@ class House
 
 		lyrics = ["the horse and the hound and the horn that belonged to ",
 							"the farmer sowing his corn that kept ",
-							"the rooster that crowed in the morn that woke "
+							"the rooster that crowed in the morn that woke ",
+							"the priest all shaven and shorn that married "
 						]
 
     case argument
@@ -21,10 +22,8 @@ class House
       result = "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     when 7
       result = "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    when 8
-      result = "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     else
-      result = "This is #{lyrics.last(argument % 9).join('')}the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+      result = "This is #{lyrics.last(argument % 8).join('')}the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     end
     result
   end
