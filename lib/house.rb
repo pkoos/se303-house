549980400f09d65cfe29a0437ff450e8b242e034
Paul Koos <paully_k5298@yahoo.com>
2022-05-30 14:17:38 -0700

    remove case when argument is 6, and refactor logic for argument selection to remove mod and use subtraction instead.

--- lib/house.rb
+++ lib/house.rb
@@ -5,7 +5,8 @@ class House
 							"the farmer sowing his corn that kept ",
 							"the rooster that crowed in the morn that woke ",
 							"the priest all shaven and shorn that married ",
-							"the man all tattered and torn that kissed "
+							"the man all tattered and torn that kissed ",
+							"the maiden all forlorn that milked "
 						]
 
     case argument
@@ -19,10 +20,8 @@ class House
       result = "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     when 5
       result = "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    when 6
-      result = "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     else
-      result = "This is #{lyrics.last(argument % 7).join('')}the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+      result = "This is #{lyrics.last(argument - 6).join('')}the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     end
     result
   end
@@ -30,4 +29,10 @@ class House
   def recite
     (1..12).map { |line_num| line(line_num) }.join("\n")
   end
+
+	def debug_recite
+		(1..12).map { |line_num| "#{line_num}: " +line(line_num) }.join("\n")
+	end
 end
+
+puts House.new.debug_recite
