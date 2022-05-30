8a85e7ea1e0efca246c2caf8d9318939bace9167
Paul Koos <paully_k5298@yahoo.com>
2022-05-30 14:18:55 -0700

    remove case when argument is 5.

--- lib/house.rb
+++ lib/house.rb
@@ -6,7 +6,8 @@ class House
 							"the rooster that crowed in the morn that woke ",
 							"the priest all shaven and shorn that married ",
 							"the man all tattered and torn that kissed ",
-							"the maiden all forlorn that milked "
+							"the maiden all forlorn that milked ",
+							"the cow with the crumpled horn that tossed "
 						]
 
     case argument
@@ -18,10 +19,8 @@ class House
       result = "This is the rat that ate the malt that lay in the house that Jack built.\n"
     when 4
       result = "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    when 5
-      result = "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     else
-      result = "This is #{lyrics.last(argument - 6).join('')}the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+      result = "This is #{lyrics.last(argument - 5).join('')}the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     end
     result
   end
