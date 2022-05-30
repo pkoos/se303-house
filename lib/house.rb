1b901045cd8ce6a7b52d106eba1b4d0094ba8676
Paul Koos <paully_k5298@yahoo.com>
2022-05-30 13:46:35 -0700

    remove case when argument is 10.

--- lib/house.rb
+++ lib/house.rb
@@ -1,7 +1,7 @@
 class House
   def line(argument)
 
-		lyrics = ["the horse and the hound and the horn that belonged to "]
+		lyrics = ["the horse and the hound and the horn that belonged to ", "the farmer sowing his corn that kept "]
 
     case argument
     when 1
@@ -22,10 +22,8 @@ class House
       result = "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     when 9
       result = "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    when 10
-      result = "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     else
-      result = "This is #{lyrics.last(argument % 11).join('')}the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+      result = "This is #{lyrics.last(argument % 10).join('')}the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     end
     result
   end
