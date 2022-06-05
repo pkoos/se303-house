7cf01247689f4acecd921bc7a93f97447138da86
Paul Koos <paully_k5298@yahoo.com>
2022-06-04 20:52:52 -0700

    add new subclass for Random House song, and initialize new subclass specifically with Random Lyrics.

--- lib/house.rb
+++ lib/house.rb
@@ -48,6 +48,13 @@ class PirateVerse < Verse
 	end
 end
 
+class RandomHouseVerse < HouseVerse
+	def initialize(number, lyrics=RandomLyrics.lyrics)
+		super
+		@lyrics = RandomLyrics.lyrics
+	end
+end
+
 class HouseLyrics
 	def self.lyrics
 		["the horse and the hound and the horn that belonged to ",
@@ -63,5 +70,23 @@ class HouseLyrics
 		"the malt that lay in ",
 		"the house that Jack built"
 	]
+	end
+end
+
+class RandomLyrics
+	def self.lyrics
+		["the horse and the hound and the horn that belonged to ",
+		"the farmer sowing his corn that kept ",
+		"the rooster that crowed in the morn that woke ",
+		"the priest all shaven and shorn that married ",
+		"the man all tattered and torn that kissed ",
+		"the maiden all forlorn that milked ",
+		"the cow with the crumpled horn that tossed ",
+		"the dog that worried ",
+		"the cat that killed ",
+		"the rat that ate ",
+		"the malt that lay in ",
+		"the house that Jack built"
+	].shuffle(random: Random.new(529821882))
 	end
 end
